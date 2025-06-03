30060764181644bb0794cb1ca9144fcdf44288cd
Yannis Ouraghi <ouraghiyannis@gmail.com>
2025-06-03 14:17:49 +0200

    feat/ chnge text

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js mathéo le meilleur !</a>
+          Welcome to <a href="https://nextjs.org">Next.js mathéo tro bo !</a>
         </h1>
 
         <p className={styles.description}>
